08e508aa75d47fec1dca65b8c388045b8c228859
Ewan <ewanrobsonn@gmail.com>
2026-07-22 20:46:31 +1000

    Properly migrated gitignore

--- build/CMakeFiles/fc-tmp/raygui/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing download step for raygui")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Dev/Custom Project/build/CMakeFiles/fc-tmp/raygui/raygui-gitclone.cmake")
-
-endblock()
--- build/CMakeFiles/fc-tmp/raygui/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for raygui")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Dev/Custom Project/build/CMakeFiles/fc-tmp/raygui/raygui-gitupdate.cmake")
-
-endblock()
